773e747c48a18ddc4993f6d4cf7a1d0e16995e07
Mugera Vincent <mugeravincent02@gmail.com>
2026-03-18 08:42:23 +0300

    second commit

--- script.js
+++ script.js
@@ -61,6 +61,105 @@ const inputLoanAmount = document.querySelector('.form_input--loan-amount');
 const inputCloseUsername = document.querySelector('.form_input--user');
 const inputClosePin = document.querySelector('.form_input--pin');
 
-////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////
-////////////////
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}</div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+////////////////// .......... LECTURES ............. ////////////////////
+
+// const curencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// ////////////////// .......... ForEach ARRAYS ............. ////////////////////
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// //for of
+// for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(`You deposited ${movement}`);
+//   } else {
+//     console.log(`You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// //forEach
+// movements.forEach(function (movement) {
+//   if (movement > 0) {
+//     console.log(`You deposited ${movement}`);
+//   } else {
+//     console.log(`You withdrew ${Math.abs(movement)}`);
+//   }
+// });
+
+// //Counter variable
+// for (const [i, mov] of movements.entries()) {
+//   if (mov > 0) {
+//     console.log(`Transfer ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Transfer ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// }
+
+// movements.forEach(function (mov, i, array) {
+//   if (mov > 0) {
+//     console.log(`Transfer ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Transfer ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// });
+
+// ////////////////// .......... ForEach MAPS ............. ////////////////////
+// const curencies1 = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// curencies1.forEach(function (value, key, map``) {
+//   console.log(`${key}: ${value}`);
+// })
+
+// const currencies2 = new Set(['USD', 'GBP', 'USD', 'EUR',]);
+// console.log(currencies2);
+// currencies2.forEach(function (value, _, map) {
+//   console.log(`${value}; ${value}`);
+// })
+
+//////////////// .......... First coding challenge ............. //////////////////
+
+const julia = [3, 5, 2, 12, 7];
+const kate = [4, 1, 15, 8, 3];
+//create a function which takes -arrays of dog's ages
+const dogAges = function (dogsJulia, dogsKate) {
+  const juliaCopy = dogsJulia.slice(1, -2);
+  const onlyDogs = [...juliaCopy, ...dogsKate];
+
+  onlyDogs.forEach(function (dogN, i, array) {
+    if (dogN >= 3) {
+      console.log(`Dog at position ${i + 1} is an adult`);
+    } else {
+      console.log(`Dog at position ${i + 1} is a puppy`);
+    }
+  });
+};
+dogAges(julia, kate);
