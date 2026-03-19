9b1ad332b0785de667ab48ac719f96cd6407051e
Mugera Vincent <mugeravincent02@gmail.com>
2026-03-19 09:30:03 +0300

    Adding the Sum of the curent balance

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Jessica Birunji',
   movements: [5000, 3400, -150, -790, -3210, -1000, 85000, -30],
   interestRate: 1.5, //%
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Steven Kakooza Williams',
   movements: [200, -200, 340, -300, -20, 50, 400, -4600],
   interestRate: 0.7, //%
   pin: 3333,
@@ -37,7 +37,7 @@ const accounts = [account1, account2, account3, account4];
 //Elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance-value');
+const labelBalance = document.querySelector('.balance__value');
 const labelSumIn = document.querySelector('.summary_value--in');
 const labelSumOut = document.querySelector('.summary_value--out');
 const labelSumInterest = document.querySelector('.summary_value--interest');
@@ -79,6 +79,25 @@ const displayMovements = function (movements) {
   });
 };
 displayMovements(account1.movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
+// Adding an element in the accounts which is the short form of their names.
+const createShortForm = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createShortForm(accounts);
 
 /////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
@@ -147,19 +166,51 @@ displayMovements(account1.movements);
 
 //////////////// .......... First coding challenge ............. //////////////////
 
-const julia = [3, 5, 2, 12, 7];
-const kate = [4, 1, 15, 8, 3];
-//create a function which takes -arrays of dog's ages
-const dogAges = function (dogsJulia, dogsKate) {
-  const juliaCopy = dogsJulia.slice(1, -2);
-  const onlyDogs = [...juliaCopy, ...dogsKate];
+// const julia = [3, 5, 2, 12, 7];
+// const kate = [4, 1, 15, 8, 3];
+// //create a function which takes -arrays of dog's ages
+// const dogAges = function (dogsJulia, dogsKate) {
+//   const juliaCopy = dogsJulia.slice(1, -2);
+//   const onlyDogs = [...juliaCopy, ...dogsKate];
 
-  onlyDogs.forEach(function (dogN, i, array) {
-    if (dogN >= 3) {
-      console.log(`Dog at position ${i + 1} is an adult`);
-    } else {
-      console.log(`Dog at position ${i + 1} is a puppy`);
-    }
-  });
-};
-dogAges(julia, kate);
+//   onlyDogs.forEach(function (dogN, i, array) {
+//     if (dogN >= 3) {
+//       console.log(`Dog at position ${i + 1} is an adult`);
+//     } else {
+//       console.log(`Dog at position ${i + 1} is a puppy`);
+//     }
+//   });
+// };
+// dogAges(julia, kate);
+
+// ////////////////// .......... MAP, FILTER REDUCE ............. ////////////////////
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// eurToUSD = 2 * movements;
+
+// const movementUSD = movements.map(function (mov) {
+//   return mov * eurToUSD;
+// });
+
+// console.log(movementUSD);
+
+//The map works as the forEach method though it returns a new array
+//without tempering the old one promoting functional programming.
+
+// const movUSD = movements.map(mov => 2 * eurToUSD);
+// console.log(movUSD);
+
+// const witd = movements.filter(mov => mov < 0);
+// console.log(witd);
+
+//Reduce which takes in two arguments, a function and an initial accumulator value.
+//Accumulator, acc is the sum
+// const balance = movements.reduce(function (accum, currValue, i, arr) {
+//   console.log(`Iteration ${i + 1}: ${currValue}: ${accum}`);
+//   return accum + currValue;
+// }, 0);
+
+// console.log(balance);
+
+///////////////////........OR........./////////////////
+// const balance5 = movements.reduce((accum, curVal) => accum + curVal);
+// console.log(balance5);
